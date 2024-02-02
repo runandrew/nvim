1e743222c0f1d2db276728b72bc63c9629652c8f
Andrew Garcia <andrew@andrewgarcia.me>
2024-02-01 20:59:48 -0800

    Formatting

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,9 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "typescript"},
+      ensure_installed = { "lua", "javascript", "typescript" },
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
