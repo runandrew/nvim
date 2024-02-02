dfff8121e967e8699e7c1093beeb6e249df2c12e
Andrew Garcia <andrew@andrewgarcia.me>
2024-02-01 21:46:23 -0800

    Unhide hidden files, fix globals

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,13 +1,22 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
     "nvim-lua/plenary.nvim",
-	  "nvim-tree/nvim-web-devicons",
-	  "MunifTanjim/nui.nvim",
-	},
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+        },
+      }
+    })
+
     vim.keymap.set("n", "<C-n>", ":Neotree toggle reveal left<CR>", {})
-		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
-	end, 
+    vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+  end,
 }
