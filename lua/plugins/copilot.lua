94dc3ba901c5a50e022787dc0c2041f070b2d3ed
Andrew Garcia <andrew@andrewgarcia.me>
2024-02-02 18:37:04 -0800

    Add Copilot

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,17 @@
+return {
+  "github/copilot.vim",
+  config = function()
+    vim.g.copilot_no_tab_map = true
+
+    local map = vim.keymap.set
+    -- Use 'tab' to accept the suggestion
+    map("i", "<C-]>", "<Plug>(copilot-accept-word)", {})
+
+    vim.g.copilot_filetypes = {
+      ["*"] = false,
+      ["javascript"] = true,
+      ["typescript"] = true,
+      ["lua"] = true,
+    }
+  end,
+}
